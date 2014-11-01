f0b26ec4a31301505789828a2ff9a5ddc37fac90
mamalias23 <mamalias23@gmail.com>
2014-11-01 20:42:49 +0800

    fix routes

--- app/routes.php
+++ app/routes.php
@@ -20,3 +20,23 @@ Route::get('debt-management', function()
 {
 	return View::make('debt-management');
 });
+
+Route::get('trust-deeds', function()
+{
+	return View::make('trust-deeds');
+});
+
+Route::get('individual-voluntary-arrangements', function()
+{
+	return View::make('individual-voluntary-arrangements');
+});
+
+Route::get('debt-arrangement-schemes', function()
+{
+	return View::make('debt-arrangement-schemes');
+});
+
+Route::get('full-finals', function()
+{
+	return View::make('full-finals');
+});
