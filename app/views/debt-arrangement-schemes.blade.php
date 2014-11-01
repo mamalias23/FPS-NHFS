7ed581eb747e13d035fe617a5202635ac36981de
mamalias23 <mamalias23@gmail.com>
2014-11-01 21:15:14 +0800

    update for some pages

--- app/views/debt-arrangement-schemes.blade.php
+++ app/views/debt-arrangement-schemes.blade.php
@@ -20,6 +20,25 @@
 	<div class="row">
 		<div class="col-md-12">
 			<h1 class="page-header">DAS – Debt Arrangement Scheme</h1>
+			<p>
+				A debt arrangement scheme is available for residents of Scotland. It allows you to repay your creditors back on an affordable 
+				monthly basis but also protects you from any further action being taken by any of your creditors. Once the DAS is agreed all 
+				interest and charges will be frozen on your debts. 
+			</p>
+
+			<h3>Benefits of a DAS</h3>
+			<ul class="custom">
+				<li>Interest and charges will be frozen</li>
+				<li>If your circumstance change your payments can be looked at and possibility of a 6 month payment break</li>
+				<li>No further legal action will be taken</li>
+			</ul>
+
+			<h3>Important things to remember</h3>
+			<ul class="custom">
+				<li>Once the DAS is setup your details will appear of the DAS register which will be online that anyone can access</li>
+				<li>As will debt management if with affect your credit file</li>
+				<li>It is only available if you are a resident in Scotland</li>
+			</ul>
 		</div>
 	</div>
 
--- app/views/individual-voluntary-arrangements.blade.php
+++ app/views/individual-voluntary-arrangements.blade.php
@@ -20,6 +20,155 @@
 	<div class="row">
 		<div class="col-md-12">
 			<h1 class="page-header">IVA – Individual Voluntary Arrangement</h1>
+
+			<p>
+				This is only available to people who live in England, Wales or Northern Ireland. 
+			</p>
+			<p>
+				An IVA is a contract negotiated with your creditors on your behalf. It is an alternative to bankruptcy where you pay back what 
+				you can afford in settlement of the debt. 
+				If you have unsecured debts of over £10,000 and are able to make a contribution towards your debts, an IVA could be the 
+				most suitable debt solution for you.  
+			</p>
+
+			<h3>Benefits of an IVA</h3>
+			<ul class="custom">
+				<li>All pressure from your creditors is removed</li>
+				<li>You will only repay what you can reasonably afford </li>
+				<li>All correspondence and queries from your creditors are dealt with on your behalf </li>
+				<li>You will make only one monthly repayment </li>
+				<li>At the end of the IVA, which is usually a 5 year period, you will be debt free from the debts included within your IVA and 
+ready to start afresh financially </li>
+			</ul>
+
+			<h3>Things you need to know about an IVA</h3>
+			<ul class="custom">
+				<li>If you fail to fulfil the terms of the IVA, this could lead to bankruptcy and your home and other assets may be at risk</li>
+				<li>Usually, a proportion of any equity in your property will need to be made available to creditors and any other significant assets may also be required to be sold. If you are required to release equity from your property, your ability to obtain a re-mortgage may be restricted. If you are unable to secure a re-mortgage to release any equity in your home, your debt solution may be extended</li>
+				<li>Your credit rating may be affected for a minimum of 6 years</li>
+				<li>You cannot borrow money during the arrangement and may find it difficult to obtain credit thereafter. There will be restrictions placed on your expenditure while you are in an IVA</li>
+				<li>A monthly administration fee will be charged and would be taken from your monthly contributions. Any fee charged would be agreed with your creditors and would be fully explained to you before you commit to this debt solution.</li>
+			</ul>
+
+			<p>
+				For more information on IVAs read our questions and answers.
+			</p>
+			<p>
+				- To succeed, the following guidelines would normally be expected:<br>
+				What are the conditions of an Individual Voluntary Arrangement?
+			</p>
+			<ul class="custom">
+				<li>3 or more lines of credit from 2 or more creditors.</li>
+				<li>Minimum unsecured debt of £10,000</li>
+				<li>Minimum monthly payments of £100</li>
+				<li>You cannot borrow money during the arrangement and may find it difficult to obtain credit thereafter. There will be restrictions placed on your expenditure while you are in an IVA</li>
+				<li>You or your partner has a regular income</li>
+			</ul>
+
+			<h3>What are the pros and cons of an IVA:</h3>
+
+			<h4>The pros:</h4>
+			<ul class="custom">
+				<li>You only pay what you can afford, based on your income and expenditure</li>
+				<li>It is an alternative to bankruptcy</li>
+				<li>Your creditors may write off a proportion of what you owe them, based on what you can afford to repay</li>
+				<li>Your creditors are not allowed to chase you for money</li>
+			</ul>
+			<h4>The cons:</h4>
+			<ul class="custom">
+				<li>An IVA agreement can be in place for up to 5 years. Bankruptcy, can be quicker – lasting between 1 and 3 years</li>
+				<li>You cannot borrow money during the arrangement and may find it difficult to obtain credit thereafter</li>
+				<li>Failure to adhere to a solution, once accepted, could result in bankruptcy</li>
+				<li>Your credit record may be affected</li>
+			</ul>
+
+			<h3>How much will an IVA cost me:</h3>
+			<p>
+				As part of the IVA process, a meeting is called and your creditors vote on your IVA proposal. At this meeting, they all agree how much your IVA provider will be paid.
+			</p>
+			<p>
+				You then make your monthly contributions and create a pot of cash for your creditors. Your creditors tell your IVA provider how much they can charge in fees and their fees will be taken from the pot. You do not receive a bill at any stage; you just pay the agreed contributions. The amount of fees payable will be fully explained to you before you commit to an IVA.
+			</p>
+
+			<h3>How do I enter an IVA:</h3>
+			<p>
+				Before you decide to enter into an IVA, you need to understand how an IVA works.
+			</p>
+			<p>
+				The whole IVA process takes around 5 weeks and you would normally go through the following stages:
+			</p>
+
+			<p>
+				<strong>INTERVIEW STAGE –</strong> you will speak to a debt consultant. They will run through your financial situation and talk through your options. They will then advise whether an IVA is right for you. If an IVA is the best solution for you, the debt consultant will capture the information they need to draft an IVA Proposal.
+			</p>
+
+			<p>
+				<strong>APPLICATION STAGE</strong> – an IVA Proposal will be drafted which states your assets and how much money you owe. It also details how much you can afford to contribute each month under an IVA and how much money your creditors can expect to receive.
+			</p>
+			<p>
+				The Proposal will estimate how much your creditors would receive if you were to go bankrupt and the draft proposal will be sent out to you, for you to read over and sign.
+			</p>
+			<p>
+				You will be asked to provide documental evidence to prove that the information you’ve provided is correct, for example, pay slips, utility bills, statements etc.
+			</p>
+
+			<p>
+				<strong>CREDITOR STAGE –</strong> when your signed Proposal is received, it is sent out to all your creditors together with a report and recommendation that they should accept the IVA Proposal.
+			</p>
+			<p>
+				IVAs can only be put together by Licensed Insolvency Practitioners (accountants who specialise in insolvency work). As professionals, Licensed Insolvency Practitioners act in everyone’s best interests (yours and your creditors) and consequently your creditors are guided by what the IP tells them, therefore, only IVA Proposals that they think will be acceptable to everyone will be recommended.
+			</p>
+
+			<p>
+				<strong>VOTING STAGE –</strong> when the proposal is sent to your creditors, a meeting is called where your proposal is discussed and your creditors vote in favour of it or against it. The meeting will take place about 3 weeks after the proposal is sent to your creditors.
+			</p>
+			<p>
+				At least 75% of your creditors must approve the IVA for it to go ahead. If it is approved, all creditors must accept the deal and are then said to be bound by it, even if they voted against it at the meeting.
+			</p>
+
+			<p>
+				<strong>SUPERVISORY STAGE –</strong> if the creditors accept the IVA, an Insolvency Practitioner becomes the Supervisor of the IVA. The Insolvency Practitioner will write to all the creditors, telling them the outcome of the meeting and then the IVA formally starts.
+			</p>
+
+			<h3>How long does an IVA last:</h3>
+			<p>
+				An IVA normally lasts for 60 months (5 Years), after which time you are left debt-free from the debts included within your IVA. You should be aware that your credit rating may be affected for a minimum of 6 years.
+			</p>
+
+			<h3>Will all my debts be included in an IVA:</h3>
+			<p>
+				Your IVA will only include your unsecured debts such as unsecured loans, credit cards, store cards, rent arrears and overdrafts. Your mortgage or any other secured loans you have will not be included.
+			</p>
+
+			<h3>Will I get to keep my house if I enter into an IVA:</h3>
+			<p>
+				Normally yes, you will get to keep your home; however, if you enter into an IVA, the agreement may require you to release any equity available in your property to pay your creditors.
+			</p>
+
+			<h3>Will anyone know I am in an IVA:</h3>
+			<p>
+				Each IVA is held on The Insolvency Service register which is publically available. However, unless you have told your friends, family or employer about your IVA, it is unlikely they would come across the database and look for your name.
+			</p>
+
+			<h3>Under an IVA are some of my debts written off:</h3>
+			<p>
+				Under a standard IVA you will pay only what you can afford to pay. The contributions you make are based on what is left of your salary after taking out reasonable living expenses. These contributions are paid into a pot over a period of time (anywhere up to 5 years), with the accumulated money than being paid out to your creditors. After the defined period of time, the debts included in the IVA are cleared or written off.
+			</p>
+
+			<h3>What happens if I miss a payment during the IVA period:</h3>
+			<p>
+				The IVA is a contract. Your creditors may have promised to write off a large proportion of the money you owe them and in return, you have promised to pay a contribution to them over a number of months. If you fail to do this you will be in breach of contract, or said to be in default.
+			</p>
+
+			<h3>What happens if I received a financial windfall during the IVA:</h3>
+			<p>
+				A windfall clause is generally included in the IVA Proposal. Whether it is redundancy money, inheritance or a lottery win, in most cases any money you receive during the term of the IVA will be paid to your creditors. You must declare any financial windfalls to your IVA provider. Even if you’re unsure whether money you receive would be classified as a windfall, it is wise to check with your IVA provider. If your financial windfall will allow you to pay off your debts and IVA fees in full plus statutory interest, then you may no longer be required to make payments and your IVA will complete early.
+			</p>
+
+			<h3>How do I contact you?</h3>
+			<p>
+				Just fill out the online enquiry form and one of our agents will arrangement an appointment to discuss your situation with you when it suits you best.
+			</p>
 		</div>
 	</div>
 
